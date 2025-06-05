e1f4d3750ec047aa9e15cfcbc6c1db53cb6c3424
Dmitriy Tverdiakov <dmitriy.tverdiakov@neo4j.com>
2025-06-05 17:50:46 +0100

    feature(queryapi): rename `query.api` package to `query_api`
    BREAKING CHANGE: this update renames the `query.api` to `query_api` and requires an adjustment of imports. The JPMS module has also been renamed.

--- neo4j-bolt-connection-query-api/src/main/java/org/neo4j/bolt/connection/query_api/impl/DiscardSummaryImpl.java
+++ neo4j-bolt-connection-query-api/src/main/java/org/neo4j/bolt/connection/query_api/impl/DiscardSummaryImpl.java
@@ -14,17 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * The Neo4j Bolt Connection Query API implementation module.
- */
-module org.neo4j.bolt.connection.query_api {
-    provides org.neo4j.bolt.connection.BoltConnectionProviderFactory with
-            org.neo4j.bolt.connection.query_api.QueryApiBoltConnectionProviderFactory;
+package org.neo4j.bolt.connection.query_api.impl;
 
-    exports org.neo4j.bolt.connection.query_api;
+import java.util.Map;
+import org.neo4j.bolt.connection.summary.DiscardSummary;
+import org.neo4j.bolt.connection.values.Value;
 
-    requires org.neo4j.bolt.connection;
-    requires java.net.http;
-    requires java.naming;
-    requires com.fasterxml.jackson.jr.ob;
-}
+record DiscardSummaryImpl(Map<String, Value> metadata) implements DiscardSummary {}
